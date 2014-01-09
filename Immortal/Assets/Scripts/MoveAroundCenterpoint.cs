04d802ad64467890c9a142b1f6cb61050a1f8684
DanGrenade <danphernbrott@gmail.com>
2014-01-09 13:29:55 -0600

    SwitchedRotateAroundPivot

---
+++ Immortal/Assets/Scripts/MoveAroundCenterpoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAroundCenterpoint : MonoBehaviour 
+{
+	public Rigidbody2D PivotRotater;
+	private float RotateSpeedScale = 0.01f;
+	private float xSpeed = 1.0f;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 hey = PivotRotater.transform.eulerAngles;
+		hey.z -= (10/(transform.position - PivotRotater.transform.position).magnitude * RotateSpeedScale) * xSpeed  * Time.deltaTime;
+		PivotRotater.transform.eulerAngles = hey;
+	}
+}
