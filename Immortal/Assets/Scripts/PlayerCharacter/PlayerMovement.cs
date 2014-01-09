14c0c20e13ee479de7877a35b5b519a935d28c8b
DanGrenade <danphernbrott@gmail.com>
2014-01-09 12:47:07 -0600

    CAPS

--- Immortal/Assets/Scripts/PlayerCharacter/PlayerMovement.cs
+++ Immortal/Assets/Scripts/PlayerCharacter/PlayerMovement.cs
@@ -5,42 +5,42 @@ public class PlayerMovement : MonoBehaviour {
 
 	private Vector2 velocityHolder;
 	private float movementValue;
-	public float drag = 0.87f;
-	public float accel = 2f;
+	public float Drag = 0.87f;
+	public float Accel = 2f;
 	public float MaximumSpeed = 6f;
 	private float inputValue;
 
-	public Rigidbody2D pivotRotater;
-	public float rotateSpeedScale = 0.01f;
+	public Rigidbody2D PivotRotater;
+	public float RotateSpeedScale = 0.01f;
 	void Update () 
 	{
 		#region Get Input
 		inputValue = 0;
 		if(Input.GetKey(KeyCode.S))
 		{
-			inputValue -= accel;
+			inputValue -= Accel;
 		}
 		if(Input.GetKey(KeyCode.W))
 		{
-			inputValue += accel;
+			inputValue += Accel;
 		}
 		#endregion
 
 		#region Apply Movement
 		movementValue += inputValue;
-		movementValue *= drag;
+		movementValue *= Drag;
 
 		if(Mathf.Abs(movementValue) > MaximumSpeed)
 		{
 			movementValue = Mathf.Sign(movementValue) * MaximumSpeed;
 		}
-		rigidbody2D.velocity = (transform.position - pivotRotater.transform.position).normalized * movementValue;
+		rigidbody2D.velocity = (transform.position - PivotRotater.transform.position).normalized * movementValue;
 		#endregion
 
 		#region Rotate around centerpoint
-		Vector3 hey = pivotRotater.transform.eulerAngles;
-		hey.z -= 10/(transform.position - pivotRotater.transform.position).magnitude * rotateSpeedScale * Time.deltaTime;
-		pivotRotater.transform.eulerAngles = hey;
+		Vector3 hey = PivotRotater.transform.eulerAngles;
+		hey.z -= 10/(transform.position - PivotRotater.transform.position).magnitude * RotateSpeedScale * Time.deltaTime;
+		PivotRotater.transform.eulerAngles = hey;
 
 
 		#endregion
