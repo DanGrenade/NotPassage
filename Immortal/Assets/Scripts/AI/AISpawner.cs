4ad45af253007534ad3a9da51da22242c57023c3
DanGrenade <danphernbrott@gmail.com>
2014-01-13 13:32:34 -0600

    SpritesForSpawnAreas

--- Immortal/Assets/Scripts/AI/AISpawner.cs
+++ Immortal/Assets/Scripts/AI/AISpawner.cs
@@ -22,100 +22,82 @@ public class AISpawner : MonoBehaviour
 	public float MaxTime;
 	private float currentTime;
 	public float[] timeForStates;
+	public Sprite[] sprites;
+	private SpriteRenderer render;
 	public float rateOfDecay;
 	#endregion
 
-	#region Growth Stuff
-	public float rateOfGrowth;
-
-	#endregion
+	private bool dead = false;
 
 	public void Start()
 	{
 		currentTime = MaxTime;
+		render = gameObject.GetComponent<SpriteRenderer> ();
 	}
 
 	public void Update()
 	{
-		#region Spawning AI
-		if (nextAISpawnTime < Time.time) 
+		if(!dead)
 		{
-			GameObject.Instantiate (AIPrefabs[Random.Range(0, AIPrefabs.Length)], 
-			                        ((new Vector2(Radius * Mathf.Cos (Random.Range(0, 360) * Mathf.Deg2Rad), Radius * Mathf.Sin (Random.Range(0, 360) * Mathf.Deg2Rad))) + (Vector2)transform.position),
-			                        Quaternion.identity);
+			#region Spawning AI
+			if (nextAISpawnTime < Time.time) 
+			{
+				GameObject.Instantiate (AIPrefabs[Random.Range(0, AIPrefabs.Length)], 
+				                        ((new Vector2(Radius * Mathf.Cos (Random.Range(0, 360) * Mathf.Deg2Rad), Radius * Mathf.Sin (Random.Range(0, 360) * Mathf.Deg2Rad))) + (Vector2)transform.position),
+				                        Quaternion.identity);
+
+				switch(currentState)
+				{
+				case AreaState.One:
+					nextAISpawnTime += TimerStates[0];
+					break;
+				case AreaState.Two:
+					nextAISpawnTime += TimerStates[1];
+					break;
+				case AreaState.Three:
+					nextAISpawnTime += TimerStates[2];
+					break;
+				case AreaState.Four:
+					nextAISpawnTime += TimerStates[3];
+					break;
+				}
+			}
+			#endregion
+
+			#region Decay
+			currentTime -= (rateOfDecay * Time.deltaTime);
 
 			switch(currentState)
 			{
 			case AreaState.One:
-				nextAISpawnTime += TimerStates[0];
+				if(currentTime < timeForStates[0])
+				{
+					currentState = AreaState.Two;
+					render.sprite = sprites[1];
+				}
 				break;
 			case AreaState.Two:
-				nextAISpawnTime += TimerStates[1];
+				if(currentTime < timeForStates[1])
+				{
+					currentState = AreaState.Three;
+					render.sprite = sprites[2];
+				}
 				break;
 			case AreaState.Three:
-				nextAISpawnTime += TimerStates[2];
+				if(currentTime < timeForStates[2])
+				{
+					currentState = AreaState.Four;
+					render.sprite = sprites[3];
+				}
 				break;
 			case AreaState.Four:
-				nextAISpawnTime += TimerStates[3];
+				if(currentTime < 0)
+				{
+					dead = true;
+				}
 				break;
 			}
-		}
-		#endregion
-
-		#region Decay
-		currentTime -= (rateOfDecay * Time.deltaTime);
-
-		switch(currentState)
-		{
-		case AreaState.One:
-			if(currentTime < timeForStates[0])
-			{
-				currentState = AreaState.Two;
-			}
-			break;
-		case AreaState.Two:
-			if(currentTime < timeForStates[1])
-			{
-				currentState = AreaState.Three;
-			}
-			else if(currentTime > timeForStates[0])
-			{
-				currentState = AreaState.One;
-			}
-			break;
-		case AreaState.Three:
-			if(currentTime < timeForStates[2])
-			{
-				currentState = AreaState.Four;
-			}
-			else if(currentTime > timeForStates[1])
-			{
-				currentState = AreaState.One;
-			}
-			break;
-		case AreaState.Four:
-			if(currentTime < 0)
-			{
-				//Destroy!
-			}
-			else if(currentTime > timeForStates[2])
-			{
-				currentState = AreaState.Three;
-			}
-			break;
-		}
-		#endregion
-	}
-
-	public void OnTriggerStay2D(Collider2D other)
-	{
-		if(other.tag == "Player")
-		{
-			currentTime += rateOfGrowth;
-			if(currentTime > MaxTime)
-			{
-				currentTime = MaxTime;
-			}
+			#endregion
 		}
 	}
 }
