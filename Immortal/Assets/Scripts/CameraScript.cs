46e2209be76bd638eec218ed0c06ec442846fe2e
Shad0wKing92 <jacob.berry@loop.colum.edu>
2014-01-13 15:57:12 -0600

    Revert "Fixed?"
    This reverts commit 44de2da94370429fde4ce5781fae88b540f1d7c5.

--- Immortal/Assets/Scripts/CameraScript.cs
+++ Immortal/Assets/Scripts/CameraScript.cs
@@ -6,57 +6,10 @@ public class CameraScript : MonoBehaviour
 
 	public GameObject Protagonist;
 
-	public bool InMenu = true;
-	public bool InGame = false;
-	
-	public float zoomInMenu;
-	public Vector2 cameraPositionInMenu;
-	public float zoomInGame;
-	public GameObject protagonist;
-	
-	public float zoomSpeed;
-	
-	
-	// Use this for initialization
-	void Start () 
-	{
-		Camera.main.orthographicSize = zoomInMenu;
-		Camera.main.transform.position = cameraPositionInMenu;
-		Time.timeScale = 0;
-	}
-	
 	// Update is called once per frame
 	void Update () 
 	{
-		if(!InGame)
-		{
-			if(InMenu)
-			{
-				if(Input.GetKeyDown(KeyCode.Return))
-				{
-					InMenu = false;
-				}
-				if(Input.GetKeyDown (KeyCode.Escape))
-				{
-					Application.Quit();
-				}
-			}
-			
-			if(!InMenu)
-			{
-				Camera.main.transform.position = Vector2.Lerp(Camera.main.transform.position, protagonist.transform.position, zoomSpeed);
-				Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, zoomInGame, zoomSpeed);
-				if(transform.position == protagonist.transform.position)
-				{
-					Camera.main.transform.parent = protagonist.transform;
-					Time.timeScale = 1;
-				}
-			}
-		}
-		else
-		{
-			transform.position = Protagonist.transform.position;
-			transform.rotation = Protagonist.transform.rotation;
-		}
+		transform.position = Protagonist.transform.position;
+		transform.rotation = Protagonist.transform.rotation;
 	}
 }
