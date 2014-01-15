101ebc1aec2108c5f725c8f6b40f8a74d3e3a378
DanGrenade <danphernbrott@gmail.com>
2014-01-15 12:30:42 -0600

    ScoreFix

--- Immortal/Assets/Scripts/PlayerCharacter/PlayerScore.cs
+++ Immortal/Assets/Scripts/PlayerCharacter/PlayerScore.cs
@@ -10,21 +10,30 @@ public class PlayerScore : MonoBehaviour
 	private float scoreDisplayedFloat = 0;
 
 	public Vector2 DisplayLocation;
-
+	private List<GameObject> numberDisplays;
+	private int tempInt;
 
 	private Sprite[] numbers;
 
 	public void Start()
 	{
-		numbers = Resources.LoadAll("ScoreFont");
+		//numbers = Resources.LoadAll("ScoreFont");
 	}
 
 	public void Update()
 	{
-		scoreDisplayedFloat = Mathf.Lerp (scoreDisplayedFloat, Score, Time.deltaTime * 0.1f);
-		scoreDisplayed = (int)scoreDisplayedFloat;
+		if(scoreDisplayed != Score)
+		{
+			scoreDisplayedFloat = Mathf.Lerp (scoreDisplayedFloat, Score, Time.deltaTime * 0.1f);
+			scoreDisplayed = (int)scoreDisplayedFloat;
+
+			/*do
+			{
 
 
+			} while(tempInt > 0);*/
+
+		}
 	}
 
 	public void AddScore(int _scoreToAdd)
