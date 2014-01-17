70461fe1e540463a085512335d2a2e388cecce4d
DanGrenade <danphernbrott@gmail.com>
2014-01-17 15:10:44 -0600

    Spin

---
+++ Immortal/Assets/Scripts/Spin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+	public float spinSpeed;
+	private Vector3 rotation;
+
+	public void Start()
+	{
+		rotation = gameObject.transform.eulerAngles;
+	}
+
+	public void Update()
+	{
+		rotation.z += spinSpeed;
+		gameObject.transform.eulerAngles = rotation;
+	}
+}
