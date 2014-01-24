5bad84de6d01314b2255277092d4b3e42b998fb0
DanGrenade <danphernbrott@gmail.com>
2014-01-24 13:38:30 -0600

    Fixed+MoreGoogleEyes

---
+++ Immortal/Assets/Scripts/Googlyeyes.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Googlyeyes : MonoBehaviour {
+
+	private Vector2 VectorToPlayer;
+	private float rotation;
+	public GameObject Protagonist;
+
+	private Vector3 rotationHolder;
+
+	private GameObject eyeOne;
+	private GameObject eyeTwo;
+
+	void Start()
+	{
+		rotationHolder = eyeOne.transform.eulerAngles;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		VectorToPlayer = transform.position - Protagonist.transform.position;
+
+		rotation = Vector2.Angle (Vector2.up, VectorToPlayer);
+		if(Protagonist.transform.position.x > transform.position.x)	rotation *= -1;
+		rotation = 360 - rotation;
+		rotation -= 90;
+		
+		rotationHolder.z = rotation;
+		eyeOne.transform.eulerAngles = rotationHolder;
+		eyeTwo.transform.eulerAngles = rotationHolder;
+	}
+}
